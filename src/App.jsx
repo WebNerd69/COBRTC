849c56518f42ff409fe480f10ec809770885f111
webNerd69 <rroy64330@gmail.com>
2024-09-23 03:50:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-zinc-950 h-screen w-screen text-white text-4xl flex justify-center items-center font-bold'>
+      COBRTC
+    </div>
+  )
+}
+
+export default App
